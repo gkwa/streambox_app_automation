2336dddbd8d16e0a6f62a9dc54a8dc4754d20069
deeptikn <alexchuong@Alexs-MacBook-Pro.local>
2014-05-28 13:53:25 -0700

    WIP: IFBHelper.js

--- src/FileHelper.js
+++ src/FileHelper.js
@@ -1,13 +1,10 @@
 #import "LoginHelper.js"
 #import "Credentials.js"
 
-
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
 var result2 = testLoginWithValidCredentialsSuccess(validUserName, validPassword)
-
-
 
 function testingTheFile() {
 
--- src/IFBHelper.js
+++ src/IFBHelper.js
@@ -2,6 +2,7 @@
 #import "Credentials.js"
 
 
+
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
@@ -10,18 +11,11 @@ var result2 = testLoginWithValidCredentialsSuccess(validUserName, validPassword)
 target.frontMostApp().mainWindow().buttons()["Settings Button   Neutral"].tap();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["IFB"].tap();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Name"].tap();
-target.frontMostApp().keyboard().typeString("fcv\n");
+target.frontMostApp().keyboard().typeString("hh\n");
 target.frontMostApp().navigationBar().rightButton().tap();
-target.frontMostApp().mainWindow().tableViews()[1].cells()["fcv"].tap();
-target.frontMostApp().mainWindow().tableViews()[1].tapWithOptions({tapOffset:{x:0.97, y:0.28}});
+target.frontMostApp().mainWindow().tableViews()[1].cells()["hh"].elements()["More info, hh"].tapWithOptions({tapOffset:{x:0.10, y:0.59}});
 target.frontMostApp().navigationBar().rightButton().tap();
-target.frontMostApp().navigationBar().leftButton().tap();
+//target.frontMostApp().navigationBar().leftButton().tap();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Decoders, Live Server"].scrollToVisible();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Meta Data"].scrollToVisible();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Logout"].tap();
-----------------------------------------------
-
-
-
-
-
