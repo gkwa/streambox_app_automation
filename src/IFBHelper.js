d593921d1bcb54c6dfc0adbad0b12fece3680910
deeptikn <alexchuong@Alexs-MacBook-Pro.local>
2014-05-28 12:29:05 -0700

    WIP:ifbhelper

--- src/IFBHelper.js
+++ src/IFBHelper.js
@@ -7,3 +7,21 @@ var app = target.frontMostApp();
 var window = app.mainWindow();
 var result2 = testLoginWithValidCredentialsSuccess(validUserName, validPassword)
 
+target.frontMostApp().mainWindow().buttons()["Settings Button   Neutral"].tap();
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["IFB"].tap();
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Name"].tap();
+target.frontMostApp().keyboard().typeString("fcv\n");
+target.frontMostApp().navigationBar().rightButton().tap();
+target.frontMostApp().mainWindow().tableViews()[1].cells()["fcv"].tap();
+target.frontMostApp().mainWindow().tableViews()[1].tapWithOptions({tapOffset:{x:0.97, y:0.28}});
+target.frontMostApp().navigationBar().rightButton().tap();
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Decoders, Live Server"].scrollToVisible();
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Meta Data"].scrollToVisible();
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Logout"].tap();
+----------------------------------------------
+
+
+
+
+
